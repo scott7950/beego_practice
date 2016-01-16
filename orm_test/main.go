5f976f6e10988f8c87ee031cfc2aaf45d647f018
scott7950 <scott0926@outlook.com>
2016-01-15 23:06:02 -0800

    insertion data into db works; have issue in User.Profile which is 0 in db

--- orm_test/main.go
+++ orm_test/main.go
@@ -1,31 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
-	_ "orm_test/models"
+	"orm_test/models"
 	_ "orm_test/routers"
 )
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
-
-	//orm.RegisterDataBase("default", "mysql", "root:root@/orm_test?charset=utf8")
 	orm.RegisterDataBase("default", "mysql", "scott:password@/orm_test?charset=utf8")
 }
 
 func main() {
+	orm.Debug = true
 
 	o := orm.NewOrm()
 	o.Using("default")
 
-	profile := new(Profile)
+	//var prof *models.Profile
+	profile := new(models.Profile)
 	profile.Age = 30
 
-	user := new(User)
+	user := new(models.User)
 	user.Profile = profile
 	user.Name = "slene"
+
+	profile.User = user
 
 	fmt.Println(o.Insert(profile))
 	fmt.Println(o.Insert(user))
